b553f9adcaffb9ce39b0e9e1348594aeb54e904d
meejah <meejah@meejah.ca>
2020-02-01 05:59:03 -0700

    more hacking

--- test_libs/pyspec/proposing_test.py
+++ test_libs/pyspec/proposing_test.py
@@ -18,8 +18,8 @@ def create_validators(balances):
 eth_to_gwei = 1000000000
 N = 100
 
-validator_balances_eth = [32, 1, 16, 16, 16, 16, 16, 16]
-validator_balances_gwei = [e * eth_to_gwei for e in validator_balances_eth]
+validator_balances_eth = [32,] + [32000 / eth_to_gwei] * 7
+validator_balances_gwei = [int(e * eth_to_gwei) for e in validator_balances_eth]
 
 state = e2.BeaconState(
     validators=create_validators(validator_balances_gwei),
@@ -39,6 +39,23 @@ total_gwei = sum(validator_balances_gwei)
 expected_ratios = [float(balance) / float(total_gwei) for balance in validator_balances_gwei]
 ratios = [float(count) / float(N) for count in counts]
 diffs = [expected - actual for expected, actual in zip(expected_ratios, ratios)]
+print(counts)
 print(expected_ratios)
 print(ratios)
 print(diffs)
+
+x = (32000 / eth_to_gwei)
+big = (32 * eth_to_gwei)
+print(x / ((x * 7) + big))
+
+
+def slash(gwei_balance):
+    slash_amt = gwei_balance // e2.MIN_SLASHING_PENALTY_QUOTIENT
+    return gwei_balance - slash_amt
+
+balance = 32 * eth_to_gwei
+count = 0
+while balance > 32000:
+    count += 1
+    balance = slash(balance)
+print("slash from 32eth to <32000 gwei: {}".format(count))
